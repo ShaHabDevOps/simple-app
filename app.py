babaa1f5f4f0af8f9ae6271c103dd1d99bb382f9
root <shahabuddinkhan2059.com>
2025-12-16 13:13:29 +0545

    Initial commit - simple app

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from shahabuddin for the rollback  Pipelin!"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
